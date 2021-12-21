8b849df5cdebc87092e9d0c21bea79f7ebfa7fd9
Tristan Alie <TristanAlie@yahoo.com>
2021-12-21 15:57:32 -0500

    did the addExpense component

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Main() {
+    const expenses = [
+        {
+            
+        }
+    ]
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/AddExpense.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+export default function AddExpense() {
+    const initialState = {
+        title: '',
+        amount: 0,
+        entity: '',
+        credit: false,
+    }
+    const[expense, setExpense] = useState(initialState)
+    const allExpenses = []
+    const handleInputChange = (e) => {
+        setExpense( ...expense, {[e.target.name]: e.target.value} )
+    }
+    const onSubmit = (e) => {
+        e.preventDefault()
+        allExpenses.push(expense)
+        console.log(allExpenses)
+        setExpense(initialState)
+    }
+
+    return (
+        <div>
+            <input type="text" name='title' value={expense.title} placeholder='Name of Expense' onChange={handleInputChange}/>
+            <input type="number" name="amount" value={expense.amount} placeholder='Amount of Expense' onChange={handleInputChange}/>
+            <input type='text' name='entity' value={expense.entity} placeholder='Who or what caused this Expense' onChange={handleInputChange}/>
+            <button onClick={(e) => {
+                e.preventDefault()
+                setExpense( ...expense, expense.credit = true)
+            }} >Is this a Credit?</button>
+            <button type='submit' onClick={} >Submit</button>
+        </div>
+    )
+}
