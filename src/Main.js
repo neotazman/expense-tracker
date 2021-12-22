1738697801fcd7c48cc1f5aca5f587dc256f776c
Tristan Alie <TristanAlie@yahoo.com>
2021-12-22 03:49:15 -0500

    it's displaying correctly, but it won't save. i think i need to do back end shit for that

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AddExpense from './components/AddExpense'
 
 export default function Main() {
@@ -8,14 +8,39 @@ export default function Main() {
             amount: 5.60,
             entity: 'Starbucks',
             credit: false,
-        }
+        },
     ]
+
+    //expense state
+    const initialState = {
+        title: '', //what the expense is
+        amount: 0, //how much 
+        entity: '', //what the expense is for
+        credit: false, //there is logic for a gain, but i already called it expense. CHANGE TO TRANSACTION TRACKER
+    }
+    const[expense, setExpense] = useState(initialState)
+
+    //drilling functions
+    const handleInputChange = (e) => { //changing the expense values
+        e.preventDefault()
+        setExpense( {...expense, [e.target.name]: e.target.value} )
+    }
+    const isCredit = (e) => { //checking if it is income
+        e.preventDefault()
+        setExpense( {...expense, credit: !expense.credit} )
+    }
+    const onSubmit = (e) => {
+        e.preventDefault()
+        expenses.push(expense) //should add the expense to the array
+        console.log(expenses, expense) //isn't adding right now, it's resetting the second index again and again
+        setExpense(initialState) //clears the state after it submits
+    }
 
 
     return (
         <div>
-            <AddExpense />
-            {expenses && expenses.map(expense => <p>{expense.title}, {expense.credit ? `+$${expense.amount}` : `-$${expense.amount}`}, {expense.entity}</p>)}
+            <AddExpense expense={expense} handleInputChange={handleInputChange} onSubmit={onSubmit} isCredit={isCredit}/>
+            {expenses && expenses.map((expense, keyNum) => <p key={keyNum} >{expense.title}, {expense.credit ? `+$${expense.amount}` : `-$${expense.amount}`}, {expense.entity}</p>)}
         </div>
     )
 }
--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,33 +1,15 @@
-import React, { useState } from 'react'
+import React from "react"
 
-export default function AddExpense() {
-    const initialState = {
-        title: '',
-        amount: 0,
-        entity: '',
-        credit: false,
-    }
-    const[expense, setExpense] = useState(initialState)
-    const allExpenses = []
-    const handleInputChange = (e) => {
-        setExpense( ...expense, {[e.target.name]: e.target.value} )
-    }
-    const onSubmit = (e) => {
-        e.preventDefault()
-        allExpenses.push(expense)
-        console.log(allExpenses)
-        setExpense(initialState)
-    }
+export default function AddExpense({expense, handleInputChange, onSubmit, isCredit}) {
+
 
     return (
         <div>
             <input type="text" name='title' value={expense.title} placeholder='Name of Expense' onChange={handleInputChange}/>
             <input type="number" name="amount" value={expense.amount} placeholder='Amount of Expense' onChange={handleInputChange}/>
-            <input type='text' name='entity' value={expense.entity} placeholder='Who or what caused this Expense' onChange={handleInputChange}/>
-            <button onClick={(e) => {
-                e.preventDefault()
-                setExpense( ...expense, expense.credit = true)
-            }} >Is this a Credit?</button>
+            <input type='text' name='entity' value={expense.entity} placeholder='Entity causing Expense' onChange={handleInputChange}/>
+            <button onClick={isCredit} >Is this a Credit?</button>
+            {expense.credit ? <span>yes</span> : <span>no</span>}
             <button type='submit' onClick={onSubmit} >Submit</button>
         </div>
     )
